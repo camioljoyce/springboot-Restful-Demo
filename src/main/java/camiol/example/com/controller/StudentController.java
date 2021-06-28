7d1bd7c693f59bec1f7c0c3fd53e1c220edf6692
Camiol <camioljoyce@gmail.com>
2021-06-28 17:30:41 +0800

    加入swagger.io來建立符合OpenAPI規範的Restful API

--- src/main/java/camiol/example/com/controller/StudentController.java
+++ src/main/java/camiol/example/com/controller/StudentController.java
@@ -14,7 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 import camiol.example.com.entity.Student;
 import camiol.example.com.service.StudentService;
 import camiol.example.com.vo.ResponseVo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
+@Api(tags = "學生相關操作")
 @RestController	
 @RequestMapping(value = "/student", produces = "application/json")
 public class StudentController {
@@ -22,6 +27,12 @@ public class StudentController {
 	@Autowired
 	private StudentService service;
 	
+	@ApiOperation(value = "取得指定學生資料" , notes = "取得指定學生資料")
+	@ApiResponses({
+		@ApiResponse(responseCode = "200" ,description = "成功"),
+		@ApiResponse(responseCode = "404" ,description = "找不到網頁"),
+		@ApiResponse(responseCode = "500" ,description = "其他錯誤")
+	})
 	@GetMapping("/get")
 	public ResponseVo getStudent(@RequestParam("id") long id) {
 		ResponseVo result = new ResponseVo();
@@ -36,6 +47,13 @@ public class StudentController {
 		}
 		return result;
 	}
+	
+	@ApiOperation(value = "取得所有學生資料" , notes = "取得所有學生資料" )
+	@ApiResponses({
+		@ApiResponse(responseCode = "200" ,description = "成功"),
+		@ApiResponse(responseCode = "404" ,description = "找不到網頁"),
+		@ApiResponse(responseCode = "500" ,description = "其他錯誤")
+	})
 	@GetMapping("/getAll")
 	public ResponseVo getAllStudent() {
 		ResponseVo result = new ResponseVo();
@@ -50,6 +68,13 @@ public class StudentController {
 		}
 		return result;
 	}
+	
+	@ApiOperation(value = "新增學生資料" , notes = "新增學生資料" )
+	@ApiResponses({
+		@ApiResponse(responseCode = "200" ,description = "成功"),
+		@ApiResponse(responseCode = "404" ,description = "找不到網頁"),
+		@ApiResponse(responseCode = "500" ,description = "其他錯誤")
+	})
 	@PostMapping("/add")
 	public ResponseVo saveStudent(@RequestParam("name") String name,@RequestParam("mathScore") int mathScore) {
 		ResponseVo result = new ResponseVo();
@@ -62,6 +87,13 @@ public class StudentController {
 		result.setRcode("0000");
 		return result;
 	}
+	
+	@ApiOperation(value = "更新學生資料" , notes = "更新學生資料" )
+	@ApiResponses({
+		@ApiResponse(responseCode = "200" ,description = "成功"),
+		@ApiResponse(responseCode = "404" ,description = "找不到網頁"),
+		@ApiResponse(responseCode = "500" ,description = "其他錯誤")
+	})
 	@PutMapping("/update")
 	public ResponseVo updateStudent(@RequestParam("id") long id,@RequestParam("name") String name,@RequestParam("mathScore") int mathScore) {
 		ResponseVo result = new ResponseVo();
@@ -79,6 +111,13 @@ public class StudentController {
 		}
 		return result;
 	}
+	
+	@ApiOperation(value = "刪除學生資料" , notes = "刪除學生資料" )
+	@ApiResponses({
+		@ApiResponse(responseCode = "200" ,description = "成功"),
+		@ApiResponse(responseCode = "404" ,description = "找不到網頁"),
+		@ApiResponse(responseCode = "500" ,description = "其他錯誤")
+	})
 	@DeleteMapping("/delete")
 	public ResponseVo deleteStudent(@RequestParam("id") long id) {
 		ResponseVo result = new ResponseVo();
--- src/main/java/camiol/example/com/entity/Student.java
+++ src/main/java/camiol/example/com/entity/Student.java
@@ -7,15 +7,24 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 @Entity
 @Table(name = "Student")
+@ApiModel(description = "學生資料")
 public class Student {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@ApiModelProperty(value = "學號")
 	private long id;
+	
 	@Column(name="Name")
+	@ApiModelProperty(value = "姓名")
 	private String name;
+	
 	@Column(name="Math_Score")
+	@ApiModelProperty(value = "數學成績")
 	private int mathScore;
 	
 	public long getId() {
